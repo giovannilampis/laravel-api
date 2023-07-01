3b268ca38d812c91579f2562668b7d9cca2b8199
giovannilampis <giolampis@hotmail.com>
2023-07-01 10:37:33 +0200

    change public function generateSlug in Project.php

--- app/Models/Admin/Project.php
+++ app/Models/Admin/Project.php
@@ -21,6 +21,11 @@ class Project extends Model
         "category_id"
     ];
 
+    public static function generateSlug($title)
+    {
+        return Str::slug($title, '-');
+    }
+
     public function category():BelongsTo{
         return $this->belongsTo(Category::class);
     }
@@ -28,17 +33,5 @@ class Project extends Model
         return $this->belongsToMany(Technology::class);
     }
 
-    public function generateSlug()
-{
-    $slug = Str::slug($this->title);
-    $count = 2;
-
-    while ($this->slugExists($slug)) {
-        $slug = Str::slug($this->title) . '-' . $count;
-        $count++;
-    }
-
-    $this->slug = $slug;
-}
-
+    
 }
